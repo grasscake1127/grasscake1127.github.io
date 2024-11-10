a236a967926f8395605710473bd9c7684f701bf4
syuaweu <105784560+syuaweu@users.noreply.github.com>
2024-11-11 01:37:58 +0800

    [Fix Bug] (1) Delete no used line in script.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", function() {
     // 當頁面完全載入後，隱藏 Loading 畫面並顯示主內容
     document.getElementById("loading-screen").style.display = "none";
-    document.getElementById("main-content").style.display = "block";
 });
 
 function random(min, max) {
