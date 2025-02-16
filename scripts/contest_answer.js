8299982832508acfd1e84bd956c114ed7e476ecd
syuaweu <105784560+syuaweu@users.noreply.github.com>
2025-02-16 19:37:40 +0800

    [Feat] Add 2nd contest's answer.

---
+++ scripts/contest_answer.js
@@ -0,0 +1,19 @@
+const question12 = document.querySelector('.ton0-12');
+const question13 = document.querySelector('.ton0-13');
+const question14 = document.querySelector('.ton0-14');
+const question15 = document.querySelector('.ton0-15');
+
+question12.addEventListener('click', ()=>{
+    question12.src = "./img/12-ans.png"
+})
+question13.addEventListener('click', ()=>{
+    question13.src = "./img/13-ans.png"
+})
+
+question14.addEventListener('click', ()=>{
+    question14.src = "./img/14-ans.PNG"
+})
+
+question15.addEventListener('click', ()=>{
+    question15.src = "./img/15-ans.png"
+})
